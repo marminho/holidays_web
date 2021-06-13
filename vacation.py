d1a1835ec70468161d5a02b1402f17ab6f8f6cc7
marminho <63997055+marminho@users.noreply.github.com>
2021-06-13 11:16:26 +0200

    updated with secrets

--- vacation.py
+++ vacation.py
@@ -4,7 +4,12 @@ import pandas as pd
 import datetime
 import requests
 
+url_correct = 'https://drive.google.com/uc?id=' + st.secrets['url'].split('/')[-2]
 
+def get_exchange(currency: str) -> float:
+	response = requests.get(st.secrets['exchange_respone'])
+	response = response.json()
+	return response['rates'][currency]
 
 ratePLN = get_exchange('PLN')
 
@@ -12,7 +17,8 @@ categories = ['Nocleg', 'Bilety', 'Transport', 'Rozrywka', 'Jedzenie']
 
 st.text('Holiday Tracker')
 
-
+#data = pd.read_excel('st.secrets['drive_loc'])
+data = pd.read_excel(url_correct)
 data = data[['Name', 'Category', 'Currency', 'Price']]
 data['Price Conv'] = [x if y == 'PLN' else x * ratePLN for x,y in zip(data['Price'], data['Currency'])]
 
@@ -22,6 +28,9 @@ data
 
 st.text('Total Amount so far:')
 total
+
+cat_split = data.groupby(['Category'])['Price Conv'].sum()
+st.bar_chart(cat_split, width = 800, use_container_width= False)
 
 cost_name = st.text_input('Input next cost name')
 cost_category = st.selectbox('Select Category', categories)
@@ -35,5 +44,6 @@ else:
 if st.button('Confirm choice'):
 
 	data = data.append({'Name' : cost_name, 'Category' : cost_category, 'Currency' : cost_currency, 'Price' : cost_value, 'Price Conv' : cost_conv}, ignore_index= True)
-
+	#data.to_excel(st.secrets['drive_loc'])
+	#data.to_excel(url_correct)
 	data
