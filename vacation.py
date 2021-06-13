f0a145fdde33f01998b2ba6030c0053a66282056
marminho <63997055+marminho@users.noreply.github.com>
2021-06-13 10:31:25 +0200

    safety

--- vacation.py
+++ vacation.py
@@ -5,10 +5,6 @@ import datetime
 import requests
 
 
-def get_exchange(currency: str) -> float:
-	response = requests.get('http://data.fixer.io/api/latest?access_key=f210cc8d9600e88cc1dc7c8a16e93a2a&format=1')
-	response = response.json()
-	return response['rates'][currency]
 
 ratePLN = get_exchange('PLN')
 
@@ -16,7 +12,6 @@ categories = ['Nocleg', 'Bilety', 'Transport', 'Rozrywka', 'Jedzenie']
 
 st.text('Holiday Tracker')
 
-data = pd.read_excel('C:\\Users\\mpiot\\Documents\\Python Scripts\\vacation\\holiday.xls')
 
 data = data[['Name', 'Category', 'Currency', 'Price']]
 data['Price Conv'] = [x if y == 'PLN' else x * ratePLN for x,y in zip(data['Price'], data['Currency'])]
@@ -40,6 +35,5 @@ else:
 if st.button('Confirm choice'):
 
 	data = data.append({'Name' : cost_name, 'Category' : cost_category, 'Currency' : cost_currency, 'Price' : cost_value, 'Price Conv' : cost_conv}, ignore_index= True)
-	data.to_excel('C:\\Users\\mpiot\\Documents\\Python Scripts\\vacation\\holiday.xls')
 
 	data
