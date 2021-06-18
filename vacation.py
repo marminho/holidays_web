e04ddf9ae89d0de4cb26a6455420b59317b5c91e
marminho <63997055+marminho@users.noreply.github.com>
2021-06-18 14:42:25 +0200

    dropbox added

--- vacation.py
+++ vacation.py
@@ -3,35 +3,37 @@ import streamlit as st
 import pandas as pd
 import datetime
 import requests
-
-#url_correct = 'https://drive.google.com/uc?id=' + st.secrets['url'].split('/')[-2]
+import dropbox
+import io
 categories = ['Nocleg', 'Bilety', 'Transport', 'Rozrywka', 'Jedzenie']
+dbx = dropbox.Dropbox(st.secrets['dropbox_token'])
+file = '/holiday.xls'
+
 
 def get_exchange(currency: str) -> float:
 	response = requests.get(st.secrets['exchange_respone'])
 	response = response.json()
 	return response['rates'][currency]
 
+def read_dropbox(dbx, file):
+    _, f = dbx.files_download(file)
+    with io.BytesIO(f.content) as stream:
+           df = pd.read_excel(stream, index_col=0)
+    return df
+
+
 ratePLN = get_exchange('PLN')
 
-
-st.text('Holiday Tracker')
-
-#data = pd.read_excel('st.secrets['drive_loc'])
-
-data = pd.read_excel('holiday.xls')
-#data = pd.read_excel(url_correct)
+data = read_dropbox(dbx, file)
 data = data[['Name', 'Category', 'Currency', 'Price']]
 data['Price Conv'] = [x if y == 'PLN' else x * ratePLN for x,y in zip(data['Price'], data['Currency'])]
-
 total = data['Price Conv'].sum()
+cat_split = data.groupby(['Category'])['Price Conv'].sum()
 
+st.text('Holiday Tracker')
 data
-
 st.text('Total Amount so far:')
 total
-
-cat_split = data.groupby(['Category'])['Price Conv'].sum()
 st.bar_chart(cat_split, width = 800, use_container_width= False)
 
 cost_name = st.text_input('Input next cost name')
@@ -46,7 +48,5 @@ else:
 if st.button('Confirm choice'):
 
 	data = data.append({'Name' : cost_name, 'Category' : cost_category, 'Currency' : cost_currency, 'Price' : cost_value, 'Price Conv' : cost_conv}, ignore_index= True)
-	#data.to_excel(st.secrets['drive_loc'])
-	#data.to_excel(url_correct)
-	data.to_excel('holiday.xls')
+	dbx.files_upload(data.to_excel('holiday.xls'), '/holiday,xls', mode = dropbox.files.WriteMode.overwrite)
 	data
